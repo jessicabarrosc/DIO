96fd36e6f84a954d83f9356dd5d305c8e792c235
Jessica Barros <jessicabarros@outlook.com.br>
2021-05-14 11:41:32 -0300

    inclusao pratica MVC

---
+++ CursoMVC/CursoMVC/Models/Produto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace CursoMVC.Models
+{
+    public class Produto
+    {
+        public int Id { get; set; }
+        [Display(Name = "Descrição")]
+        [Required(ErrorMessage = "Campo obrigatório")]
+        public string Descricao { get; set; }
+        public int Quantidade { get; set; }
+
+        public int CategoriaId { get; set; }
+        public Categoria Categoria { get; set; }
+
+    }
+}
---
+++ CursoMVC/CursoTeste/CategoriaController.cs
@@ -0,0 +1,36 @@
+﻿using CursoMVC.Models;
+using CursoAPI.;
+using Microsoft.EntityFrameworkCore;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace CursoTeste
+{
+    public class CategoriaController
+    {
+        private readonly Mock<DbSet<Categoria>> _mockSet;
+        private readonly Mock<Context> _mockContext;
+        private readonly Categoria _categoria;
+
+        public CategoriaController(Context @object)
+        {
+            _mockSet = new Mock<DbSet<Categoria>>();
+            _mockContext = new Mock<Context>();
+            _categoria = new Categoria { Id = 1, Descricao = "Teste Categoria" };
+        }
+
+        [Fact]
+
+        public async Task Get_Categoria()
+        {
+            var service = new CategoriaController(_mockContext.Object);
+            await service.GetCategoria(1);
+            _mockSet.Verify(m => m.FindAsync(1), Times.Once());
+        }
+
+    }
+}
